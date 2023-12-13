fea2b884bb4c90dec351ca57d72376bcccfdde28
chocobubble <kyaya12345@gmail.com>
2023-12-14 08:29:56 +0900

    231214_data_protobuf

--- WebServer/obj/Debug/net7.0/WebServer.AssemblyInfo.cs
+++ WebServer/obj/Debug/net7.0/WebServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a044a0dc7d02184b00b820dd33b1f63b71c5fae5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
