1bcafb53cdc768843d5be932b09d9fa578a42e1f
chocobubble <kyaya12345@gmail.com>
2023-12-17 11:04:27 +0900

    언리얼 연동 #5

--- WebServer/Controllers/ProtoController.cs
+++ WebServer/Controllers/ProtoController.cs
@@ -51,24 +51,110 @@ namespace WebServer.Controllers
         [HttpGet]
         public byte[] ProtoTest1()
         {
-            CharacterData characterData = new CharacterData();
-            WeaponSaveData weaponSaveData = new WeaponSaveData();
-            weaponSaveData.WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle;
-            weaponSaveData.WeaponLevel = 2;
-            weaponSaveData.WeaponEnhancementLevel = 3;
-            characterData.Level = 3;
-            characterData.Exp = 5;
-            characterData.PlayerName = "palyer";
-            characterData.Gold = 100;
-            //characterData.WeaponSaveData = weaponSaveData;
-            characterData.WeaponSaveData = new WeaponSaveData();
-            characterData.WeaponSaveData.WeaponLevel = 2;
-            characterData.WeaponSaveData.WeaponEnhancementLevel = 3;
-            characterData.WeaponSaveData.WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle;
-            characterData.RifleAmmo = 1000;
+            //CharacterData characterData = new CharacterData();
+            //WeaponSaveData weaponSaveData = new WeaponSaveData();
+            //weaponSaveData.WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle;
+            //weaponSaveData.WeaponLevel = 2;
+            //weaponSaveData.WeaponEnhancementLevel = 3;
+            //characterData.Level = 3;
+            //characterData.Exp = 5;
+            //characterData.PlayerName = "palyer";
+            //characterData.Gold = 100;
+            ////characterData.WeaponSaveData = weaponSaveData;
+            //characterData.WeaponSaveData = new WeaponSaveData();
+            //characterData.WeaponSaveData.WeaponLevel = 2;
+            //characterData.WeaponSaveData.WeaponEnhancementLevel = 3;
+            //characterData.WeaponSaveData.WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle;
+            //characterData.RifleAmmo = 1000;
             //byte[] bytes = Encoding.UTF8.GetBytes("prototo");
+
+            CharacterData characterData = new CharacterData
+            {
+                Level = 3,
+                Exp = 5,
+                PlayerName = "player",
+                Gold = 100,
+                WeaponSaveData = new WeaponSaveData { WeaponLevel = 2, WeaponEnhancementLevel = 3, WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle },
+                RifleAmmo = 1000
+            };
             byte[] bytes = characterData.ToByteArray();
             return bytes;
+        }
+
+        [HttpGet]
+        public ByteString ProtoTest2()
+        {
+            //CharacterData characterData = new CharacterData();
+            //WeaponSaveData weaponSaveData = new WeaponSaveData();
+            //weaponSaveData.WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle;
+            //weaponSaveData.WeaponLevel = 2;
+            //weaponSaveData.WeaponEnhancementLevel = 3;
+            //characterData.Level = 3;
+            //characterData.Exp = 5;
+            //characterData.PlayerName = "palyer";
+            //characterData.Gold = 100;
+            ////characterData.WeaponSaveData = weaponSaveData;
+            //characterData.WeaponSaveData = new WeaponSaveData();
+            //characterData.WeaponSaveData.WeaponLevel = 2;
+            //characterData.WeaponSaveData.WeaponEnhancementLevel = 3;
+            //characterData.WeaponSaveData.WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle;
+            //characterData.RifleAmmo = 1000;
+            //byte[] bytes = Encoding.UTF8.GetBytes("prototo");
+
+            CharacterData characterData = new CharacterData
+            {
+                Level = 3,
+                Exp = 5,
+                PlayerName = "player",
+                Gold = 100,
+                WeaponSaveData = new WeaponSaveData { WeaponLevel = 2, WeaponEnhancementLevel = 3, WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle },
+                RifleAmmo = 1000
+            };
+            ByteString byteString =  characterData.ToByteString();
+            return byteString;
+        }
+
+        [HttpGet]
+        public String ProtoTest3()
+        {
+
+            CharacterData characterData = new CharacterData
+            {
+                Level = 3,
+                Exp = 5,
+                PlayerName = "player",
+                Gold = 100,
+                WeaponSaveData = new WeaponSaveData { WeaponLevel = 2, WeaponEnhancementLevel = 3, WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle },
+                RifleAmmo = 1000
+            };
+            return characterData.ToString();
+        }
+
+        [HttpGet]
+        public byte[] ProtoTest4()
+        {
+            ProtoTest protoTest = new ProtoTest
+            {
+                Num = 1
+            };
+            byte[] bytes = protoTest.ToByteArray();
+            return bytes;
+        }
+
+        [HttpGet]
+        public ByteString ProtoTest5()
+        {
+            CharacterData characterData = new CharacterData
+            {
+                Level = 3,
+                Exp = 5,
+                PlayerName = "player",
+                Gold = 100,
+                WeaponSaveData = new WeaponSaveData { WeaponLevel = 2, WeaponEnhancementLevel = 3, WeaponType = WeaponSaveData.Types.WeaponType.EwtRifle },
+                RifleAmmo = 1000
+            };
+            ByteString byteString = characterData.ToByteString();
+            return byteString;
         }
     }
 }
