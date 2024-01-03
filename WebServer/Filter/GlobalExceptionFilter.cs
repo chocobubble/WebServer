99a3673d10ec96e896f601fde2df5328b4395446
Moon <100405650+chocobubble@users.noreply.github.com>
2024-01-04 08:39:52 +0900

    240104_filter #10

---
+++ WebServer/Filter/GlobalExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WebServer.HttpCommand;
+using ProtoBuf;
+using System.Text;
+
+namespace WebServer.Filter
+{
+	public class GlobalExceptionFilter : IExceptionFilter
+	{
+		public GlobalExceptionFilter()
+		{
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            Console.WriteLine("GlobalExceptionFilter - OnException");
+            //throw new NotImplementedException();
+
+
+            BaseResponse baseResponse = new BaseResponse();
+            baseResponse.apiReturnCode = ApiReturnCode.InvalidSessionId;
+
+            //context.Result = new ContentResult() { Content = JsonSerializer.Serialize(baseResponse) };
+            byte[] stream;
+            MemoryStream memoryStream = new MemoryStream();
+            Serializer.Serialize(memoryStream, baseResponse);
+            stream = memoryStream.ToArray();
+            context.Result = new ContentResult() { Content = Encoding.Default.GetString(stream) };
+
+        }
+    }
+}
+
